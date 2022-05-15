3eda094ea49c12c9a58df4d2034b5c28bf32debb
Andres Correa <andres.correa@websummit.com>
2022-05-16 00:00:16 +0100

    Making bootstrap use scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-require("bootstrap/dist/js/bootstrap")
+import 'bootstrap'
 
 Rails.start()
 Turbolinks.start()
