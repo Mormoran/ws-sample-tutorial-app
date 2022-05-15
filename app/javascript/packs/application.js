a44b47378bd28df0b1fa040d7e224fa4636cebeb
Andres Correa <andres.correa@websummit.com>
2022-05-15 23:32:56 +0100

    Removing plain import for more specific path one

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
-import "bootstrap"
 import 'src/application.scss'
 import 'bootstrap/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
