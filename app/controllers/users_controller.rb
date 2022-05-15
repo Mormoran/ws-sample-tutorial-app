e9ecfb17d6a03dc76a3cb4715d6ac9007367565c
Andres Correa <andres.correa@websummit.com>
2022-05-15 20:21:36 +0100

    Added a single user show controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@
 
 # Control all actions related to Users within the site
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   end
 end
