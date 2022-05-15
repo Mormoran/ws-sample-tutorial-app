3058306c408360b62e05bf5c955d9c39e261afae
Andres Correa <andres.correa@websummit.com>
2022-05-15 19:17:04 +0100

    Added users incl. password

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Handle user model properties such as validations, etc
+class User < ApplicationRecord
+  before_save :downcase_email
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  private
+
+  # Converts email to all lowercase.
+  def downcase_email
+    self.email = email.downcase
+  end
+end
