86c9384728b4131ffcedf8fce62b616946ed89e8
Andres Correa <andres.correa@websummit.com>
2022-05-15 23:37:55 +0100

    Removing bootstrap?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 Rails.start()
 Turbolinks.start()
